7aca2351bbbd0cc7fbadb8e2fd0c90227c9d9ecb
Chavisky D <auukunn.bkk@gmail.com>
2026-01-23 18:34:51 +0700

    ebug: add boot status for liff

--- scGlamLiFF/src/main.jsx
+++ scGlamLiFF/src/main.jsx
@@ -4,6 +4,17 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeProvider } from "./ThemeContext";
 
+if (typeof window !== "undefined") {
+  if (!window.__SCGLAM_BOOT__) {
+    window.__SCGLAM_BOOT__ = {};
+  }
+  window.__SCGLAM_BOOT__.jsStarted = true;
+  const bootEl = document.getElementById("boot-status");
+  if (bootEl) {
+    bootEl.style.display = "none";
+  }
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>
